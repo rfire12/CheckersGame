71fd542e4319b72abc45a08022cf3b183d735641
Raul <rapraulra@gmail.com>
2018-06-03 22:20:48 -0400

    Encapsulamiento de atributos de las clases

--- checkers.py
+++ checkers.py
@@ -1,26 +1,33 @@
 import piece
 
-#Funcion: make_table()
-#Parametros: Ninguno
-#Objetivo: Crear el tablero de damas
-#Retorno: Lista con objetos de tipo 'check'
-def make_table():
-    table = []
-    row = 0
-    char_in_ascii = 65 #Caracter ascii de la letra 'A'
-    while row < 8:
-        col = 0
-        table.append([])
-        while col < 8:
-            if (((row + col) % 2) == 0) and (row < 3 or row > 4):
-                char = str(chr(char_in_ascii)) #Convertir un numero a un string
-                fic = piece.Ficha(char)
-                table[row].append(fic)
-                char_in_ascii += 1
-            else:
-                table[row].append('-')
-            if row == 3: #Cambiar a minusculas cuando termine con las fichas mayusculas
-                char_in_ascii = 97 #Caracter ascii de la letra 'a'
-            col += 1
-        row += 1
-    return table
+# Funcion: make_table()
+# Parametros: Ninguno
+# Objetivo: Crear el tablero de damas
+# Retorno: Lista con objetos de tipo 'check'
+class Checkers():
+    def __init__(self):
+        self.__table = self.make_table()
+
+    def make_table(self):
+        table = []
+        row = 0
+        char_in_ascii = 65 #Caracter ascii de la letra 'A'
+        while row < 8:
+            col = 0
+            table.append([])
+            while col < 8:
+                if (((row + col) % 2) == 0) and (row < 3 or row > 4):
+                    char = str(chr(char_in_ascii)) #Convertir un numero a un string
+                    fic = piece.Piece(char)
+                    table[row].append(fic)
+                    char_in_ascii += 1
+                else:
+                    table[row].append('-')
+                if row == 3: #Cambiar a minusculas cuando termine con las fichas mayusculas
+                    char_in_ascii = 97 #Caracter ascii de la letra 'a'
+                col += 1
+            row += 1
+        return table
+
+    def get_table(self):
+        return self.__table
--- piece.py
+++ piece.py
@@ -1,14 +1,15 @@
-class Ficha():
+class Piece():
     def __init__(self, mask):
-        self.mask = mask
-        self.type = self.determine_type(mask)
+        self.__mask = mask
+        self.__type = self.determine_type(mask)
 
     def __repr__(self):
+        return self.__mask
 
-        return self.mask
-
-    def determine_type(mask):
+    def determine_type(self,mask):
         type = 2
         if ord(mask) >= 97: #Si el ascii de mask es > a 97 es una letra minuscula
             type = 1
         return type
+
+
