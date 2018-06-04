e6429016ff445b4c2d18b3b001fb79f8db5fa379
Raul <rapraulra@gmail.com>
2018-06-03 21:34:56 -0400

    Se anade variable type a la clase piece

--- piece.py
+++ piece.py
@@ -1,6 +1,14 @@
 class Ficha():
     def __init__(self, mask):
         self.mask = mask
+        self.type = self.determine_type(mask)
 
     def __repr__(self):
+
         return self.mask
+
+    def determine_type(mask):
+        type = 2
+        if ord(mask) >= 97: #Si el ascii de mask es > a 97 es una letra minuscula
+            type = 1
+        return type
