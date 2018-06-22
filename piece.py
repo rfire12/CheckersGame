38bc251fa7d314bcbd3b58d768c674982ec795e3
Raul <rapraulra@gmail.com>
2018-06-22 10:09:42 -0400

    Se agregan atributos a la pieza

--- piece.py
+++ piece.py
@@ -2,6 +2,8 @@ class Piece(object):
     def __init__(self, mask):
         self.__mask = mask
         self.__queen = False
+        self.__posX = None
+        self.__posY = None
 
     def __repr__(self):
         return self.__mask
@@ -18,6 +20,18 @@ class Piece(object):
     def set_queen(self):
         self.__queen = True
         self.set_mask()
+
+    def set_pos_x(self, posX):
+        self.__posX = posX
+
+    def set_pos_y(self, posY):
+        self.__posY = posY
+
+    def get_pos_x(self):
+        return self.__posX
+
+    def get_pos_y(self):
+        return self.__posY
 
 
 
